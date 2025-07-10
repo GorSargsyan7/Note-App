ce56f5fb1caf3767a1bb0e0dc0739d74cd795177
GorSargsyan7 <gor.sargsyan15@tumo.org>
2025-07-10 19:46:12 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<RouterView/>
+<div v-for="pix in arr">
+<PixCounter  v-if="pix.age <= 10" :name="pix.name" :age="pix.age" :count="pix.count"/>
+</div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import PixCounter from './components/PixCounter.vue'
+import { ref } from 'vue';
+
+let arr = ref([
+  {
+    name:'Luna',
+    age: 5,
+    count: 1
+  },
+    {
+    name:'Ronaldo',
+    age: 40,
+    count: 1
+  },
+    {
+    name:'Aaaa',
+    age: 59,
+    count: 2
+  },
+])
+</script>
+
+<style>
+
+</style>
